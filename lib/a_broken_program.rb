949c48a4c10f5fd48e35d3c3fbb48e23ff4b140e
andy-aguilar <aaguil3@gmail.com>
2020-10-15 10:35:33 -0500

    Done.

--- lib/a_broken_program.rb
+++ lib/a_broken_program.rb
@@ -1,7 +1,7 @@
 # What is a Program?
 
 puts "This is a program"
-puts "Programs are interpreted at runtime."
+#puts "Programs are interpreted at runtime."
 puts "Which means even though I'm writing this Tue Jan 17 2012 8:36am"
 puts "the time now is "+ Time.now.to_s
 
